d3b13e9c48aecbd1c1b9b3dc6c39b143106f0700
phi-jkim <jinhaworld@gmail.com>
2025-08-28 13:32:10 +0900

    Added fixes to gated delta product bwd

--- fla/ops/gated_delta_product/chunk.py
+++ fla/ops/gated_delta_product/chunk.py
@@ -99,8 +99,6 @@ def chunk_gated_delta_product_bwd(
     g_interleaved: torch.Tensor,
     beta: torch.Tensor,
     A: torch.Tensor,
-    h: torch.Tensor,
-    v_new: torch.Tensor,
     do: torch.Tensor,
     dht: torch.Tensor,
     scale: float,
@@ -108,109 +106,35 @@ def chunk_gated_delta_product_bwd(
     initial_state: Optional[torch.Tensor] = None,
     num_householder: int = 1,
 ):
-    # from fla.ops.gated_delta_product.chunk_deltaproduct_h import chunk_gated_delta_product_bwd_dhu
-    # from fla.ops.gated_delta_product.chunk_deltaproduct_o import chunk_gated_delta_product_bwd_o
-
-    # cu_seqlens_dp = cu_seqlens * num_householder if cu_seqlens is not None else None
-
-    # # compute gradients wrt q, k, v
-    # # dv_new_o is gradient w.r.t. v_new
-    # # might not be fully parallelizable due to gradient Q including previous states H_0 to H_T
-    # dq_o, dv_new_o, dk_o, dh  = chunk_gated_delta_product_bwd_o(
-    #     q=q,
-    #     k=k,
-    #     v=v_new, # v_new = U[i] - W[i]H[i]^T
-    #     h=h,
-    #     g=g, # forward_h takes in g instead of g_interleaved
-    #     scale=scale,
-    #     cu_seqlens=cu_seqlens,
-    #     num_householder=num_householder,
-    #     do=do, # gradient of the output
-    # )
-
-    # # recompute w, u from WY representation to compute gradient
-    # from fla.ops.gated_delta_rule.wy_fast import recompute_w_u_fwd as gdn_recompute_w_u_fwd
-    # from fla.ops.delta_rule.wy_fast import recompute_w_u_fwd as dn_recompute_w_u_fwd
-
-    # if g_interleaved is not None:
-    #     w, u = gdn_recompute_w_u_fwd(
-    #         k=k, v=v, beta=beta, A=A, g=g_interleaved, cu_seqlens=cu_seqlens_dp,
-    #     )
-    # else:
-    #     w, u = dn_recompute_w_u_fwd(
-    #         k=k, v=v, beta=beta, A=A, cu_seqlens=cu_seqlens_dp,
-    #     )
-
-    # # compute gradients with respect to u and w
-    # # but need to account for gradients used for sequential computation of hidden states of H_0 to H_T (sequential)
-    # dh0, du, dw = chunk_gated_delta_product_bwd_dhu(
-    #     q=q,
-    #     k=k,
-    #     w=w,
-    #     u=u,
-    #     g=g_interleaved,
-    #     initial_state=initial_state, #H_0
-    #     cu_seqlens=cu_seqlens_dp,
-    #     num_householder=num_householder,
-    #     dht=dht, # gradient w.r.t to last hidden state
-    #     dv=dv_new_o,  # gradient w.r.t. v_new
-    #     scale=scale,
-    # )
-
-    # # compute gradients w.r.t. WY representation (dk, dv, dbeta, dg)
-    # # This involves computing gradients through the Householder transformations
-    # from fla.ops.gated_delta_rule.wy_fast import prepare_wy_repr_bwd
-
-    # # gradient descent from W and U
-    # # g is used for computing W and U in the forward pass
-    # # dk2 accounts for the gradient of hidden state  wrt to k
-    # # dv_final is the  gradient of hidden state wrt to v
-    # # this can be fully parallelized
-    # dk2, dv, dbeta, dg = prepare_wy_repr_bwd(
-    #     k=k,
-    #     v=v,
-    #     beta=beta,
-    #     g=g_interleaved,
-    #     A=A,
-    #     dw=dw,  # Use key gradients from output as weights gradients
-    #     du=du,  # Use value gradients from hidden state backward
-    #     cu_seqlens=cu_seqlens_dp,
-    # )
-
-    # # accumulate gradients
-    # # dk_final = dk_o + dk2
-    # # dk_final = dk2 # should there be (Q[i] K[i]^T \cdot M)
-    # dv_final = dv
-    # dg_final = dg
-
-    # # process gating gradients with local cumsum (reverse)
-    # if g is not None:
-    #     from fla.ops.utils import chunk_local_cumsum
-    #     dg_final = chunk_local_cumsum(dg_final, chunk_size=64, reverse=True, cu_seqlens=cu_seqlens_dp)
-
-    #     # Convert interleaved gating gradients back to original format
-    #     dg_final = rearrange(dg_final, 'b (l n) h -> b l n h', n=num_householder)[:, :, 0].contiguous()
-    # else:
-    #     dg_final = None
-
-    # return dq_o, dk, dv_final, dg_final, dbeta, dh0
+    q_new = q.new_zeros(q.shape[0], q.shape[1], num_householder, q.shape[2], q.shape[3])
+    q_new[:, :, -1] = q
+    do_new = do.new_zeros(do.shape[0], do.shape[1], num_householder, do.shape[2], do.shape[3])
+    do_new[:, :, -1] = do
+    q_org, q = q, rearrange(q_new, 'b t n h d -> b (t n) h d')
+    do = rearrange(do_new, 'b t n h d -> b (t n) h d')
 
     from fla.ops.gated_delta_product.chunk_deltaproduct_h import chunk_gated_delta_product_bwd_dhu
 
     cu_seqlens_dp = cu_seqlens * num_householder if cu_seqlens is not None else None
 
     # recompute w, u from WY representation to compute gradient
-    from fla.ops.delta_rule.wy_fast import recompute_w_u_fwd as dn_recompute_w_u_fwd
-    from fla.ops.gated_delta_rule.wy_fast import recompute_w_u_fwd as gdn_recompute_w_u_fwd
+    # from fla.ops.delta_rule.wy_fast import recompute_w_u_fwd as dn_recompute_w_u_fwd
+    from fla.ops.gated_delta_rule.wy_fast import recompute_w_u_fwd 
 
-    if g_interleaved is not None:
-        w, u = gdn_recompute_w_u_fwd(
-            k=k, v=v, beta=beta, A=A, g=g_interleaved, cu_seqlens=cu_seqlens_dp,
-        )
-    else:
-        w, u = dn_recompute_w_u_fwd(
-            k=k, v=v, beta=beta, A=A, cu_seqlens=cu_seqlens_dp,
-        )
+    w, u = recompute_w_u_fwd(
+        k=k, v=v, beta=beta, A=A, g=g_interleaved, cu_seqlens=cu_seqlens_dp,
+    )
+
+    h, v_new, _ = chunk_gated_delta_product_fwd_h(
+        k=k,
+        w=w,
+        u=u,
+        g=g_interleaved,
+        initial_state=initial_state,
+        output_final_state=False,
+        cu_seqlens=cu_seqlens_dp,
+        num_householder=num_householder,
+    )
 
     # dv_new is gradient w.r.t. v_new
     # Eq: Q[i] @ H[i] + (Q[i]K[i]^T ⊙ M) * v_new[i]
@@ -293,11 +217,10 @@ def chunk_gated_delta_product_bwd(
     # by product rule add the two together
     dv_final = dv
     dg_final = dg2
-    if dg_local is not None:
-        dg_final = dg_final + dg_local  # dL/dg = dL/dO * dO/dg + dL/dO * dO/dv_new * dv_new/dg = dg_local + dg2
-
+    
     # process gating gradients with local cumsum (reverse)
     if g is not None:
+        dg_final = dg_final + dg_local  # dL/dg = dL/dO * dO/dg + dL/dO * dO/dv_new * dv_new/dg = dg_local + dg2
         from fla.ops.utils import chunk_local_cumsum
         dg_final = chunk_local_cumsum(dg_final, chunk_size=64, reverse=True, cu_seqlens=cu_seqlens_dp)
 
@@ -370,7 +293,7 @@ class ChunkGatedDeltaProductFunction(torch.autograd.Function):
         # # call the gated deltanet kernel for now.
         # # TODO: optimize the backward pass like the forward pass.
         # if g is not None:
-        #     dq, dk, dv, db, dg, dh0 = chunk_gated_delta_rule_bwd(
+            # dq, dk, dv, db, dg, dh0 = chunk_gated_delta_rule_bwd(
         #         q=q,
         #         k=k,
         #         v=v,
@@ -408,34 +331,13 @@ class ChunkGatedDeltaProductFunction(torch.autograd.Function):
         q, q_rstd, k, k_rstd, v, g, beta, A, initial_state, cu_seqlens = ctx.saved_tensors
 
         # recompute forward intermediate values
-        from fla.ops.delta_rule.wy_fast import recompute_w_u_fwd as dn_recompute_w_u_fwd
         from fla.ops.gated_delta_product.chunk_deltaproduct_h import chunk_gated_delta_product_fwd_h
-        from fla.ops.gated_delta_rule.wy_fast import recompute_w_u_fwd as gdn_recompute_w_u_fwd
+        from fla.ops.gated_delta_rule.wy_fast import recompute_w_u_fwd 
 
         cu_seqlens_dp = cu_seqlens * ctx.num_householder if cu_seqlens is not None else None
 
-        # recompute w, u from WY representation (gated and non-gated)
-        if g is not None:
-            w, u = gdn_recompute_w_u_fwd(
-                k=k, v=v, beta=beta, A=A, g=g, cu_seqlens=cu_seqlens_dp,
-            )
-        else:
-            w, u = dn_recompute_w_u_fwd(
-                k=k, v=v, beta=beta, A=A, cu_seqlens=cu_seqlens_dp,
-            )
-
         # recompute h and v_new from hidden state computation (why can't we store this originally in ctx)
         # v_new = (U[t] - W[t]H[t]^T)
-        h, v_new, _ = chunk_gated_delta_product_fwd_h(
-            k=k,
-            w=w,
-            u=u,
-            g=g,
-            initial_state=initial_state,
-            output_final_state=False,
-            cu_seqlens=cu_seqlens_dp,
-            num_householder=ctx.num_householder,
-        )
 
         # need to get g and g_interleaved
         if g is not None:
@@ -459,8 +361,6 @@ class ChunkGatedDeltaProductFunction(torch.autograd.Function):
             g_interleaved=g_interleaved,  # use computed g_interleaved
             beta=beta,
             A=A,
-            h=h,
-            v_new=v_new,
             do=do,
             dht=dht,
             scale=ctx.scale,
